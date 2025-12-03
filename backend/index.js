5064a1fc5ada25e1936711a20c7789ae48eb877b
Komal-Bhurse <komalbhurse3@gmail.com>
2025-12-03 09:11:58 +0530

    resolve cors error

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,7 @@
 import express from 'express'
-import cors from 'cors'
 import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
-
+import { cors } from './middlewares/cors.js'
 import connectMongodb from './utils/db-connection.js'
 
 import clinicRoutes from './routes/clinic.js'
@@ -17,14 +16,24 @@ const PORT = process.env.PORT || 3001
 
 connectMongodb()
 
-const corsOptions = {
-  origin: process.env.CORS_ORIGIN,
-  credentials: true,
-  methods: process.env.CORS_METHOD,
-  allowedHeaders:["Content-Type", "Authorization"]
+// const corsOptions = {
+//   origin: process.env.CORS_ORIGIN,
+//   credentials: true,
+//   methods: process.env.CORS_METHOD,
+//   allowedHeaders:["Content-Type", "Authorization"]
+// }
+
+// app.use(cors(corsOptions));
+
+const cors = (req,res,next) =>{
+        res.setHeader('Access-Control-Allow-Origin','https://procohat-frontend-plum.vercel.app');
+        res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
+        res.setHeader('Access-Control-Allow-Headers','Content-Type');
+        res.setHeader('Access-Control-Allow-Credentials',true);
+        next();
 }
 
-app.use(cors(corsOptions));
+app.use(cors);
 app.use(cookieParser())
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
