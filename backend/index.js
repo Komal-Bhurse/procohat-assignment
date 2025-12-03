de8fadc992eed083d501941dd3644cfa9bd60311
Komal-Bhurse <komalbhurse3@gmail.com>
2025-12-03 09:16:07 +0530

    resolve cors error

--- backend/index.js
+++ backend/index.js
@@ -27,13 +27,13 @@ connectMongodb()
 
 const cors = (req,res,next) =>{
         res.setHeader('Access-Control-Allow-Origin','https://procohat-frontend-plum.vercel.app');
-        res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
+        res.setHeader('Access-Control-Allow-Methods','OPTIONS,HEAD,GET,POST,PUT,PATCH,DELETE');
         res.setHeader('Access-Control-Allow-Headers','Content-Type');
         res.setHeader('Access-Control-Allow-Credentials',true);
         next();
 }
 
-app.use(cors);
+app.use(cors)
 app.use(cookieParser())
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
